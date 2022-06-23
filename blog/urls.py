3a7dd9eef42bdc87ae02254183a575d0c65f14e3
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-23 20:15:38 +0100

    view changed to viewset

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import BlogView
 
 router = DefaultRouter()
-# router.register('blogposts', BlogView, basename='blogposts')
+router.register('blogposts', BlogView, basename='blogposts')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('blogposts/', BlogView.as_view())
+    # path('blogposts/', BlogView.as_view())
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.core.paginator import Paginator
+from rest_framework import viewsets
 
 from .models import BlogModel
 from .serializers import BlogSerializer
@@ -15,48 +16,50 @@ from .serializers import BlogSerializer
 
 # Create your views here.
 
-class BlogView(APIView):
+class BlogView(viewsets.ModelViewSet):
+    queryset = BlogModel.objects.all()
+    serializer_class = BlogSerializer
 
-    def get(self, request):
-        blog = BlogModel.objects.all()
-        page_number = self.request.query_params.get('page_number', 1)
-        page_size = self.request.query_params.get('page_size', 1)
-        paginator = Paginator(blog, page_size)
-        serializer = BlogSerializer(paginator.page(page_number), many=True, context={'request': request})
-
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class BlogDetails(APIView):
-    def get_object(self, pk):
-        try:
-            return BlogModel.objects.get(pk=pk)
-        except BlogModel.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        blog = self.get_object(pk)
-        serializer = BlogSerializer(blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response
-
-    def put(self, request, id):
-        blog = self.get_object(id)
-        serializer = BlogSerializer(blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        blog = self.get_object(pk)
-        blog.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+#     def get(self, request):
+#         blog = BlogModel.objects.all()
+#         page_number = self.request.query_params.get('page_number', 1)
+#         page_size = self.request.query_params.get('page_size', 1)
+#         paginator = Paginator(blog, page_size)
+#         serializer = BlogSerializer(paginator.page(page_number), many=True, context={'request': request})
+#
+#         return Response(serializer.data)
+#
+#     def post(self, request):
+#         serializer = BlogSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class BlogDetails(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return BlogModel.objects.get(pk=pk)
+#         except BlogModel.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk):
+#         blog = self.get_object(pk)
+#         serializer = BlogSerializer(blog, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response
+#
+#     def put(self, request, id):
+#         blog = self.get_object(id)
+#         serializer = BlogSerializer(blog, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk):
+#         blog = self.get_object(pk)
+#         blog.delete()
+#         return HttpResponse(status=status.HTTP_204_NO_CONTENT)
