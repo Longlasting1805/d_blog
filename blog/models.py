f3d10dadd96bd03da0005666ddc232d8ee7b583c
Akande Al-ameen <longlasting1805@gmail.com>
2022-06-30 11:30:40 +0100

    author adjustment

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class BlogModel(models.Model):
-    author = models.CharField('author', max_length=100, null=True)
+    author = models.CharField(max_length=100, null=True)
     title = models.CharField(max_length=200, null=True)
     content = models.CharField(max_length=100, null=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
